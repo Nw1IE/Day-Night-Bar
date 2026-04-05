f8dcd34b851a0ba289727cb1967eb67ef2c8f0b9
Nw1IE <eosipov446@gmail.com>
2026-04-05 15:40:06 +0500

    White

--- js/main.js
+++ js/main.js
@@ -559,3 +559,16 @@ const menuItems = [
             };
             return categories[categoryKey] || categoryKey;
         }
+
+        const themeBtn = document.getElementById('ChangeButton');
+themeBtn.addEventListener('click', () => {
+    document.body.classList.toggle('light-theme');
+    const isLight = document.body.classList.contains('light-theme');
+    localStorage.setItem('theme', isLight ? 'light' : 'dark');
+});
+
+// При загрузке проверяем сохраненную тему
+if (localStorage.getItem('theme') === 'light') {
+    document.body.classList.add('light-theme');
+}
+
